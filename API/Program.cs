fcb4404586981424f7604b80294e26e0ac1b7058
Sanjula Ranasinghe <Sanjula.Ranasinghe@softlogiclife.lk>
2024-11-09 00:09:28 +0530

    Commit 2 - Session 25

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,10 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 // builder.Services.AddEndpointsApiExplorer();
@@ -28,6 +32,9 @@ var app = builder.Build();
 // app.UseHttpsRedirection();
 
 // app.UseAuthorization();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
